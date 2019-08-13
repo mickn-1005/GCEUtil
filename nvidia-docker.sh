0574511b6c485f7ba2fa3df2ca6a5e269acbfb30
mickn-1005 <wataru1005mikkun@gmail.com>
2019-08-13 17:11:56 +0900

    スクリプト

--- nvidia-docker.sh
+++ nvidia-docker.sh
@@ -1,12 +1,31 @@
-# 参考文献 https://qiita.com/cvusk/items/e34b19f4c112a1b11b76
-sudo su -
+#!/bin/bash
+# GCEでまっさらな状態からDockerのインストールとTensorflowイメージでの実行までを行う．
+# 参考文献 https://cloud.google.com/compute/docs/gpus/add-gpus?hl=ja#install-driver-script
 
-apt-get -y install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+echo "Checking for CUDA and installing."
+# Check for CUDA and try to install.
+if ! dpkg-query -W cuda-10-0; then
+  # The 16.04 installer works with 16.10.
+  curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
+  dpkg -i ./cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
+  apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+  apt-get update
+  apt-get install cuda-10-0 -y
+fi
+# Enable persistence mode
+nvidia-smi -pm 1
 
-apt-get -y update
-apt-get -y install docker-ce
+echo "Test code running."
+nvidia-smi
+
+
+
+echo "install tensorflow latest-gpu-jupyter"
+docker pull tensorflow/tensorflow:latest-gpu-jupyter
+
+echo "testrunning nvidia-docker"
+docker run --runtime=nvidia --rm nvidia/cuda nvidia-smi
+
+echo "test running tensrflow image"
+docker run --runtime=nvidia -it --rm tensorflow/tensorflow:latest-gpu-py3 \
+   python -c "import tensorflow as tf; tf.enable_eager_execution(); print(tf.reduce_sum(tf.random_normal([1000, 1000])))"
